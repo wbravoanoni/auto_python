73e4f9935f4405c25bd3ea97b95de84dde8d172a
Winston Bravo <wbravoanoni@gmail.com>
2018-08-07 19:59:53 -0400

    templates

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from flask import request
+from flask import request 		  #para parametros
+from flask import render_template #para llamar templates
 
 app = Flask(__name__) #nuevo objeto
+#app = Flask(__name__,template_folder='prueba_template') #nuevo objeto
 
 @app.route('/') #wrap o un decorador
 def index():
@@ -14,11 +16,16 @@ def params():
 	param = request.args.get('param1','no tiene este parametro')
 	param2 = request.args.get('param2','no tiene este parametro')
 	param3 = request.args.get('param3','no tiene este parametro')
-	return 'El parametro es Nombre: {} Apellido: {} Edad: {}'.format(param,param2,param3) #Regresa un string	
+	return 'El parametro es Nombre: {} Apellido: {} Edad: {}'.format(param,param2,param3) #Regresa un string
+
 @app.route('/parametros/')
 @app.route('/parametros/<name>/<int:num>') #wrap o un decorador
 def parametros(name ='Este es un valor por defecto',num='nada'):
 	return 'El parametro es Nombre: {} Edad: {} '.format(name,num) #Regresa un string	
+
+@app.route('/template/')
+def template():
+	return render_template('index.html')
 
 if __name__ == '__main__':
 		app.run(debug = True, port = 8000) #se encarga de ejecutar el servidor 5000
